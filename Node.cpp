bc7e3eafe191fe08f9e34dfdf9689a99c2779eb6
richardzhang121 <richard.zhang121@gmail.com>
2017-04-02 13:27:15 -0700

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,22 +1,41 @@
+#include <iostream>
+#include "Node.h"
+//A Node for a stack that holds either a char or int and a pointer to the next node
+
 using namespace std;
 
+Node::Node(char d){
+  next = NULL;
+  data.c = d;
+  type = 1;
+}
+Node::Node(int d){
+  next = NULL;
+  data.i = d;
+  type = 2;
+}
+//deconstructor, clear memory
+Node::~Node(){
 
-#include "Node.h"
-#include <iostream>
-
-Node::Node(char val){
-	this->val = val;
-	next = NULL;
-};
-
+}
+//returns a pointer to the next node
 Node* Node::getNext(){
-	return next;
-};
-
-char Node::getVal(){
-	return val;
-};
-
-void Node::setNext(Node* node){
-	next = node;
-};
+  return next;
+}
+//sets the pointer to the next node
+void Node::setNext(Node* n){
+  next = n;
+}
+//returns a pointer to the Student class
+int Node::getIntData(){
+  return data.i;
+}
+char Node::getCharData(){
+  if (type == 1){
+    return data.c;
+  }
+  else return 0;
+}
+int Node::getType(){
+  return type;
+}
